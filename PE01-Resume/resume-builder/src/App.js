8d78fe6ee1ec03f0c0e68c954f3f1d0aa93e96a0
Gayatri Soni <54921052+gayatrisoni@users.noreply.github.com>
2023-07-07 08:21:08 +0000

    submission for PE01

---
+++ PE01-Resume/resume-builder/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Resume from './Resume';
+
+function App() {
+  return (
+    <div className="App">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
